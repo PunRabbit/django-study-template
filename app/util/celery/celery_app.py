b3f6f9191e9b87cb68f47ce42d4b535dcadf19a6
PunRabbit <tjdmsch2201@gmail.com>
2024-06-09 23:33:58 +0900

    try

--- app/util/celery/celery_app.py
+++ app/util/celery/celery_app.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 from app.core.constant import CONSTANT
 
@@ -12,11 +13,11 @@ celery_app.config_from_object(
 )
 celery_app.autodiscover_tasks()
 
-# celery_app.conf.beat_schedule = {
-#     'add-every-30-seconds': {
-#         'task': 'myapp.tasks.add',
-#         'schedule': 30.0,
-#         'args': (16, 16),
-#     },
-# }
-
+celery_app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'myapp.tasks.add',
+        # 'schedule': 30.0,
+        'schedule': crontab(hour='7', minute='30', day_of_week='1'), # 매주 월요일 7시 30분에 실행
+        'args': (16, 16),
+    },
+}
