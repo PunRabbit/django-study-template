83d14af715f5e5bebb599484e1bcfa60f15ae168
PunRabbit <tjdmsch2201@gmail.com>
2024-06-10 13:30:45 +0900

    fix some docker logics

--- app/core/settings.py
+++ app/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-sjv5y-0p+m$jr$p5m6gvgo4w_ea79bi!c0#e2alu=#zcx%&j1*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
