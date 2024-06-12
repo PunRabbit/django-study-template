72359d1445d218a91186149300d374b2b1cd9723
PunRabbit <tjdmsch2201@gmail.com>
2024-06-13 01:21:34 +0900

    gogo

--- app/apps/movie/container.py
+++ app/apps/movie/container.py
@@ -13,6 +13,9 @@ class MovieContainer(DeclarativeContainer):
 
     service: MovieServiceInterface = Singleton(MovieApplication, _verify_manager)
 
+    def get_test_verify_manager(self) -> MovieVerifyInterface:
+        return self._verify_manager
+
     # wiring_config = WiringConfiguration(
     #     modules=['app.apps.movie.service.application.application', 'app.apps.movie.views']
     # )
--- app/apps/movie/test/test_module.py
+++ app/apps/movie/test/test_module.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from unittest.mock import patch
 
-from app.apps.movie.service.interface.module.repo import MovieRepoInterface
 from app.apps.movie.service.interface.module.verify import MovieVerifyInterface
 from app.apps.movie.container import MovieContainer
 
@@ -9,4 +8,5 @@ from app.apps.movie.container import MovieContainer
 class MovieModuleTest(TestCase):
     def setUp(self) -> None:
         self.container: MovieContainer = MovieContainer()
+        self.verify_manager: MovieVerifyInterface = self.container.get_test_verify_manager()
 
